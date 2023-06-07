7528473098f7ed3c21f2c64d384ad5bce49ce3b2
grhoads172 <gina.rhoads@devilbisshc.com>
2023-06-07 09:02:28 -0400

    The first of many

---
+++ Tests/the_tester/Form1.cs
@@ -0,0 +1,36 @@
+namespace the_tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+}
