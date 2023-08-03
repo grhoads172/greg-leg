6263be8e0ae4ad37e492ef45eecae16c6624b61d
grhoads172 <gina.rhoads@devilbisshc.com>
2023-08-03 15:46:55 -0400

    changed back to forward slash in psh file

--- the-testerScript.ps1
+++ the-testerScript.ps1
@@ -1,18 +1,18 @@
-$directPath = "C:\Users\rhoadg\Documents\GitHub\greg-leg"
+$directPath = "C:/Users/rhoadg/Documents/GitHub/greg-leg"
 $mainExecutable = "$directPath/Tests/the_tester/bin/Release/net6.0-windows/the_tester.exe"
 $version = "1.0"
 
-&"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\" "$directPath/Tests/Tests.sln" /property:Configuration=Release /p:Version=$version
+&"C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild" "$directPath/Tests/Tests.sln" /property:Configuration=Release /p:Version=$version
 
-&"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "$directPath/test-setup.iss"
+&"C:/Program Files (x86)/Inno Setup 6/ISCC.exe" "$directPath/test-setup.iss"
 
-if (Test-Path "C:\Users\rhoadg\Documents\GitHub\greg-leg/Installer/$version") 
+if (Test-Path "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version") 
 {
     Write-Host "Folder Exists - Not Creating it"
 }
 else 
 {
-    New-Item -Path "C:\Users\rhoadg\Documents\GitHub\greg-leg/Installer/$version" -ItemType directory
+    New-Item -Path "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version" -ItemType directory
 }
 
-Copy-Item $mainExecutable -Destination "C:\Users\rhoadg\Documents\GitHub\greg-leg/Installer/$version/the_tester.exe"
+Copy-Item $mainExecutable -Destination "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version/the_tester.exe"
