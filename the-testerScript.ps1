21958e6ddec9e5fb8eb0df47d735d14566197435
grhoads172 <gina.rhoads@devilbisshc.com>
2023-08-10 09:04:59 -0400

    no more directpath var, just straight up path

--- the-testerScript.ps1
+++ the-testerScript.ps1
@@ -3,9 +3,9 @@ $directPath = "C:/Users/rhoadg/Documents/GitHub/greg-leg"
 $mainExecutable = "$directPath/Tests/the_tester/bin/Release/net6.0-windows/the_tester.exe"
 $version = "1.0"
 
-&"C:/Program Files/Microsoft Visual Studio/2022/Enterprise/MSBuild/Current/Bin/MSBuild.exe" "\\?\C:/Users/rhoadg/Documents/GitHub/greg-leg/Tests/the_tester/the_tester.csproj" /property:Configuration=Release /p:Version=$version
+&"C:/Program Files/Microsoft Visual Studio/2022/Enterprise/MSBuild/Current/Bin/MSBuild.exe" "C:/Users/rhoadg/Documents/GitHub/greg-leg/Tests/the_tester/the_tester.csproj" /property:Configuration=Release /p:Version=$version
 
-&"C:/Program Files (x86)/Inno Setup 6/ISCC.exe" "$directPath/test-setup.iss"
+&"C:/Program Files (x86)/Inno Setup 6/ISCC.exe" "C:/Users/rhoadg/Documents/GitHub/greg-leg/test-setup.iss"
 
 if (Test-Path "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version") 
 {
