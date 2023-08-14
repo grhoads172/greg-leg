4b75231ff03281346975611710d17e9cff578e55
grhoads172 <gina.rhoads@devilbisshc.com>
2023-08-14 08:10:38 -0400

    updated directPath with the github workspace

--- the-testerScript.ps1
+++ the-testerScript.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-$directPath = "C:/Users/rhoadg/Documents/GitHub/greg-leg"
+$directPath = $env:GITHUB_WORKSPACE
 $mainExecutable = "$directPath/Tests/the_tester/bin/Release/net6.0-windows/the_tester.exe"
 $version = "1.1"
 
@@ -7,13 +7,13 @@ $version = "1.1"
 
 &"C:/Program Files (x86)/Inno Setup 6/ISCC.exe" "$directPath/test-setup.iss"
 
-if (Test-Path "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version") 
+if (Test-Path "$directPath/Installer/$version") 
 {
     Write-Host "Folder Exists - Not Creating it"
 }
 else 
 {
-    New-Item -Path "C:/Users/rhoadg/Documents/GitHub/greg-leg/Installer/$version" -ItemType directory
+    New-Item -Path "$directPath/Installer/$version" -ItemType directory
 }
 
 Copy-Item $mainExecutable -Destination "$directPath/Installer/$version/the_tester.exe"
